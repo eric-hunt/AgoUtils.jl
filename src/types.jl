42ae1762c70209f364a900134af59a01326349fc
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2022-09-29 16:14:45 -0400

    restructured to single parametric guide type

--- src/types.jl
+++ src/types.jl
@@ -2,40 +2,26 @@
 Types
 """
 
-import BioSequences
-
-
-"""
-Abstract guide type
-"""
-abstract type Guide end # abstract Guide
+using BioSequences: LongSequence, LongNuc, LongDNA, LongRNA,
+    NucleicAcidAlphabet, DNAAlphabet, RNAAlphabet
+using BioSymbols: BioSymbols.NucleicAcid, BioSymbols.DNA, BioSymbols.RNA
 
 
 """
 Concrete guide types
 """
-struct GuideDNA <: Guide
-    seq::Union{BioSequences.LongDNA, Nothing}
-    firstbase::Union{BioSequences.DNA, Nothing}
-    altbases::Union{Vector{BioSequences.DNA}, Nothing}
-    altseqs::Union{Vector{BioSequences.LongDNA}, Nothing}
-end # struct GuideDNA
+struct NucleicAcidGuide{A <: NucleicAcidAlphabet} 
+    seq::Union{LongSequence{A}, Nothing}
+    firstbase::Union{NucleicAcid, Nothing}
+    altbases::Union{Vector{NucleicAcid}, Nothing}
+    altseqs::Union{Vector{LongSequence{A}}, Nothing}
+end # struct Guide
 
-struct GuideRNA <: Guide
-    seq::Union{BioSequences.LongRNA, Nothing}
-    firstbase::Union{BioSequences.RNA, Nothing}
-    altbases::Union{Vector{BioSequences.RNA}, Nothing}
-    altseqs::Union{Vector{BioSequences.LongRNA}, Nothing}
-end # struct GuideRNA
+"An alias for NucleicAcidGuide{A <: NucleicAcidAlphabet{N}}"
+const Guide{A} = NucleicAcidGuide{A}
 
+"An alias for NucleicAcidGuide{DNAAlphabet{N}}"
+const GuideDNA{N} = NucleicAcidGuide{DNAAlphabet{N}}
 
-"""
-Traits
-"""
-abstract type GuideTrait end
-struct IsDNA <: GuideTrait end
-struct IsRNA <: GuideTrait end
-
-GuideTrait(::Type) = IsDNA() # default
-GuideTrait(::Type{<: BioSequences.LongDNA}) = IsDNA()
-GuideTrait(::Type{<: BioSequences.LongRNA}) = IsRNA()
+"An alias for NucleicAcidGuide{RNAAlphabet{N}}"
+const GuideRNA{N} = NucleicAcidGuide{RNAAlphabet{N}}
