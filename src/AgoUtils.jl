cf118e055dd539f48ec0e468b49ee95fbbcdd443
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2022-10-07 11:17:50 -0400

    added exported function to save compiled guides to CSV

--- src/AgoUtils.jl
+++ src/AgoUtils.jl
@@ -4,8 +4,9 @@ export
     # Types
     NucleicAcidGuide, Guide, GuideDNA, GuideRNA,
     # Methods
-    makeguides,
     compileguides,
+    exportguides,
+    makeguides,
     # From other package namespaces needed to use AgoUtils.jl
     DNAAlphabet,
     RNAAlphabet,
@@ -18,7 +19,9 @@ include("printing.jl")
 
 using BioSequences: @dna_str, @rna_str, LongSequence, LongNuc,
     NucleicAcidAlphabet, DNAAlphabet, RNAAlphabet, NucleicAcid
+using CSV: CSV
 using DataFrames: DataFrames, DataFrame
+using Dates: Dates
 
 
 """
@@ -55,5 +58,23 @@ function compileguides(guides::Vector{NucleicAcidGuide})
     guidetable = DataFrame(guidedict)
     return guidetable
 end # function compileguides
+
+
+function exportguides(guides::Vector{NucleicAcidGuide})
+    guidetable = compileguides(guides)
+    timestamp = Dates.format(Dates.now(), "yymmdd_HHMM")
+    CSV.write(
+        expanduser("~/Desktop/guides_$(timestamp).csv"),
+        guidetable
+    )
+end
+
+function exportguides(guidetable::DataFrame)
+    timestamp = Dates.format(Dates.now(), "yymmdd_HHMM")
+    CSV.write(
+        expanduser("~/Desktop/guides_$(timestamp).csv"),
+        guidetable
+    )
+end
 
 end # module
